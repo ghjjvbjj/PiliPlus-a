fa7c83b0b48ebc2dd9fe336a2a7d3f53f7388738
ghjjvbjj <132781660+ghjjvbjj@users.noreply.github.com>
2026-03-03 22:37:24 +0800

    Create enum file for defining play repeat types for home and collection videos

---
+++ src/types/PlayRepeatType.ts
@@ -0,0 +1,10 @@
+// Enum for defining play repeat types
+
+enum PlayRepeatType {
+    // Play repeat types for home videos
+    HomeVideo,
+    // Play repeat types for collection videos
+    CollectionVideo
+}
+
+export default PlayRepeatType;
